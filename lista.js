4682c31c9f87711346f5df10b847b3518902af6b
CrashHosaka <142937805+CrashHosaka@users.noreply.github.com>
2023-12-15 14:29:17 -0300

    Add files via upload

---
+++ lista.js
@@ -0,0 +1,46 @@
+const btnAdicionar = document.querySelector('#btnAdicionar');
+
+btnAdicionar.addEventListener("click",function() {
+    const item = document.createElement("li");
+
+    const caixaValor = document.querySelector('#minhaCaixa').value;
+
+    const novaTarefa = document.createTextNode(caixaValor);
+
+    item.appendChild(novaTarefa);
+
+    if (caixaValor === "") {
+        alert("Você deve informar uma nova tarefa para adicionar");
+    }else {
+        document.querySelector('#tarefas').appendChild(item);
+    }
+    document.querySelector('#minhaCaixa').value = "";
+
+    const span = document.createElement("span");
+
+    const icon = document.createTextNode("\u00d7");
+
+    span.className = "fechar";
+
+    span.appendChild(icon);
+
+    item.appendChild(span);
+
+    const close = document.querySelectorAll(".fechar");
+
+    for (let i = 0;i < close.length; i++){
+        close[i].addEventListener("click",function(){
+            const item = this.parentElement;
+
+            item.style.display = "none";
+        });
+    }
+})
+
+const list = document.querySelector("ul");
+
+list.addEventListener("click", function(ev) {
+    if(ev.target.nodeName === "li"){
+        ev.target.classList.tonggle("checado");
+    }
+})
